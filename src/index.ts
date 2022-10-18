87e4b9b5cd216f7025b92ad1c6b0592f59d8716f
moozzyk <moozzyk@gmail.com>
2022-10-17 22:08:23 -0700

    Simple test

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,21 @@
-console.log("Hello, World!");
+import * as puppeteer from "puppeteer";
+
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto(
+    "https://seattle.craigslist.org/search/cta?query=blazer%20k5",
+    {
+      waitUntil: "networkidle0",
+    }
+  );
+  let elements = await page.$$("a.post-title");
+  console.log(elements.length);
+  await Promise.all(
+    elements.map(async (e) => {
+      let href = await e.getProperty("href");
+      console.log(await href.jsonValue());
+    })
+  );
+  await browser.close();
+})();
