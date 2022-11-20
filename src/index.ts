95f8ba24284990929212e0c226bebe91b0e3abad
moozzyk <moozzyk@gmail.com>
2022-11-19 23:35:34 -0800

    Add boats category

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,13 @@ import { GalleryPost } from "./types";
 import { getGalleryPosts } from "./internal";
 import { BoatsForSaleFilter, CarsAndTrucksForSaleFilter } from "./filters";
 
-// export async function* forSaleBoats(site: string, filter: BoatsForSaleFilter?) {
-//   throw "Not Implemented";
-//   // return getAsyncIterator(site, "cta", );
-// }
+export async function* forSaleBoats(
+  site: string,
+  filter: BoatsForSaleFilter,
+  area?: string
+): AsyncIterableIterator<GalleryPost> {
+  yield* getGalleryPosts(site, "boo", area, filter);
+}
 
 export async function* forSaleCarsAndTrucks(
   site: string,
@@ -15,4 +18,4 @@ export async function* forSaleCarsAndTrucks(
   yield* getGalleryPosts(site, "cta", area, filter);
 }
 
-export type { CarsAndTrucksForSaleFilter } from "./filters";
+export type { CarsAndTrucksForSaleFilter, BoatsForSaleFilter } from "./filters";
